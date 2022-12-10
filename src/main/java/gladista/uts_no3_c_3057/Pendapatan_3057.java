4e7ffe2d7d1937db6f9ec146bb8815b38761169f
gladistapdr <gladistapdr18@gmail.com>
2022-12-10 21:39:40 +0700

    UTS_NO3_3057_REVISI

---
+++ src/main/java/gladista/uts_no3_c_3057/Pendapatan_3057.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package gladista.uts_no3_c_3057;
+
+/**
+ *
+ * gladista poskomalasari dwi rifan (21103057)
+ */
+public interface Pendapatan_3057 {
+    public double totalPendapatan_3057();
+}
